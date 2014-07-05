b10d5c861e3e299096c55e75cd417b7091fc5a9c
unknown <yangjin6103@hanmail.net>
2014-07-05 18:03:52 +0900

    dafasf

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Welcome Express!' });
+  res.render('index', { title: 'helllllllllo world!' });
 });
 
 module.exports = router;
